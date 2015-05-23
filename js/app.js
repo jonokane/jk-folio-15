9ae8eb2f5c45296c02b61e09de67e53d6a3ae3f2
jonokane <jono@jonokane.com>
2015-05-23 16:05:23 -0700

    adding folio items, first stab

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
 $(document).foundation();
+
+
+    docReady(function() {
+
+        var container = document.querySelector('#folio-grid');
+        var msnry = new Masonry(container, {
+            // columnWidth: 280,
+            isFitWidth: true,
+            isInitLayout: false
+        });
+
+        msnry._isLayoutInited = true;
+        msnry.layout();
+
+
+        //smooth scroll
+        $(function() {
+            $('.smoothscroll').click(function() {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                    var $target = $(this.hash);
+                    $target = $target.length ? $target : $('[name=' + this.hash.slice(1) + ']');
+                    if ($target.length) {
+                        var targetOffset = $target.offset().top - 80;
+                        $('html,body').animate({
+                            scrollTop: targetOffset
+                        }, 600, "easeInOutSine");
+                        return false;
+                    }
+                }
+            });
+        });
+
+    });
