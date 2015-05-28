bb71dceb18145afdf010a734587bb4e54c86b18d
jonokane <jono@jonokane.com>
2015-05-28 07:59:35 -0700

    updating many folio items, adding emailer, and converting index to php

---
+++ index.php
@@ -0,0 +1,299 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jonokane.com - Full-stack designer</title>
+    <link rel="stylesheet" href="css/ss-social-circle.css">
+    <link rel="stylesheet" href="css/ss-glyphish-outlined.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="bower_components/slick.js/slick/slick.css">
+    <link rel="stylesheet" href="bower_components/slick.js/slick/slick-theme.css">
+    <script src="//use.typekit.net/lvb2uax.js"></script>
+    <script>
+    try {
+        Typekit.load();
+    } catch (e) {}
+    </script>
+</head>
+
+<body id="body">
+    <div class="fixed">
+        <nav class="top-bar" data-topbar role="navigation">
+            <ul class="title-area">
+                <li class="name">
+                    <h1 class="fancy"><a href="#">jonokane.com</a></h1>
+                </li>
+                <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+                <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+            </ul>
+            <section class="top-bar-section">
+                <!-- Right Nav Section -->
+                <ul class="right">
+                    <li><a href="https://www.linkedin.com/in/jonokane" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">linkedin</span></a></li>
+                    <li><a href="http://www.twitter.com/nokapixel" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">twitter</span></a></li>
+                    <li><a href="http://facebook.com/jonokane" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">facebook</span></a></li>
+                    <li><a href="http://www.flickr.com/photos/nokapixel" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">flickr</span></a></li>
+                    <!-- <li><a href="#"><span class="ss-icon ss-social-circle">aboutme</span></a></li> -->
+                    <!-- <li><a href="#"><span class="ss-icon ss-social-circle">soundcloud</span></a></li> -->
+                    <!-- <li class="has-dropdown">
+                    <a href="#">Right Button Dropdown</a>
+                    <ul class="dropdown">
+                        <li><a href="#">First link in dropdown</a></li>
+                        <li class="active"><a href="#">Active link in dropdown</a></li>
+                    </ul>
+                </li> -->
+                </ul>
+                <!-- Left Nav Section -->
+                <ul class="left">
+                    <li><a href="#me" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">smile</span>Me</a></li>
+                    <li><a href="#view-work" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">compose</span>Work</a></li>
+                    <li><a href="#contact" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">send</span>Contact</a></li>
+                </ul>
+            </section>
+        </nav>
+    </div>
+    <div class="main-bg">
+        <section class="hero-intro">
+            <div class="row">
+                <div class="large-12 columns" id="me">
+                    <div class="hero fade-in one">
+                        <h2 class="fancy">Hi There!</h2>
+                        <p>I'm <a href="http://about.me/jonokane" target="_blank">Jono Kane</a>, a San Francisco-based UX professional. Specifically, I'm a full-stack product designer, who enjoys solving product and interaction design challenges, as well as prototyping and building front-ends for mobile, web, and tablet.</p>
+                        <p>I have a passion for creating exciting and unique media experiences on all screens. My career is primarily focused on mobile-native, mobile-web and desktop experiences.
+                        </p>
+                        <p>I also produce <a href="https://soundcloud.com/nokamusic" target="_blank">electronic music</a> and take <a href="http://flickr.com/photos/nokapixel" target="_blank">pictures</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="main-start">
+            <div class="row">
+                <div class="large-12 columns">
+                    <h4 class="fancy">Looking for a great product design lead?</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-5 large-offset-1 columns">
+                    <a href="#view-work" class="btn ghost smoothscroll">Check Out My Work</a>
+                </div>
+                <div class="large-5 columns">
+                    <a href="#contact" class="btn ghost smoothscroll">Hit Me Up</a>
+                </div>
+                <div class="large-1 columns">
+                </div>
+            </div>
+        </section>
+        <section class="content-area">
+            <div class="row">
+                <div class="large-12 columns" id="view-work">
+                    <h3 class="fancy">Work</h3>
+                    <div class="row">
+                        <div class="masonry js-masonry" id="folio-grid">
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-fluence-marketing">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/fluence-marketing-thumb.png')"></div>
+                                    <h5>Fluence.io (Website)</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-fluence-app">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/fluence-app-thumb.jpg')"></div>
+                                    <h5>Fluence.io (Web App)</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-shift">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/shift-thumb.jpg')"></div>
+                                    <h5>Shift</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-saratoga-speed">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/saratoga-speed-thumb.jpg')"></div>
+                                    <h5>Saratoga Speed</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-telenav">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/telenav-thumb.jpg')"></div>
+                                    <h5>Telenav</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-huddleup">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/huddleup-thumb.jpg')"></div>
+                                    <h5>HuddleUp</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-breeze">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/breeze-wire-thumb.jpg')"></div>
+                                    <h5>Breeze</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-intuit">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/intuit-thumb.jpg')"></div>
+                                    <h5>Intuit</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-ybubbles">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/huddleup-thumb.jpg')"></div>
+                                    <h5>Yahoo! Bubbles</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-thingsly">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/huddleup-thumb.jpg')"></div>
+                                    <h5>Thingsly</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-ydn">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/huddleup-thumb.jpg')"></div>
+                                    <h5>Yahoo! YDN</h5>
+                                </div>
+                            </a>
+                            <a href="#" data-reveal-id="modal-box" class="modal-cta folio-ydpl">
+                                <div class="item">
+                                    <div class="thumb" style="background-image:url('img/work/huddleup-thumb.jpg')"></div>
+                                    <h5>Yahoo! Design Patterns</h5>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="content-area">
+            <div class="row">
+                <div class="large-12 columns" id="contact">
+                    <h3 class="fancy">Contact</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="medium-10 medium-offset-1 columns">
+                    <p class="text-center">Say hello! <span style="font-size:1.5rem;line-height:1.5rem;">👋</span> All fields are required.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="medium-10 medium-offset-1 columns contact-form">
+                    <div id="contactMessage"></div>
+                    <form name="contact_form" id="JqAjaxForm">
+                        <div class="row">
+                            <div class="medium-6 columns">
+                                <label>Your Name
+                                    <input type="text" placeholder="What's your name?" required id="name">
+                                </label>
+                            </div>
+                            <div class="medium-6 columns">
+                                <label>Email
+                                    <input type="email" placeholder="And what's your email?" required id="email">
+                                </label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="medium-12 columns">
+                                <label>Message
+                                    <textarea placeholder="So, what's up?" rows="5" required id="message"></textarea>
+                                </label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="medium-10 medium-offset-1 columns">
+                                <button class="btn ghost" value="submit">Say Hello</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div class="row">
+                <div class="large-2 columns">
+                    <h6>jonokane.com</h6>
+                    <ul class="site-nav">
+                        <li><a href="#me" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">smile</span>Me</a></li>
+                        <li><a href="#view-work" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">compose</span>Work</a></li>
+                        <li><a href="#contact" class="smoothscroll"><span class="ss-icon ss-glyphish-outline">send</span>Contact</a></li>
+                    </ul>
+                </div>
+                <div class="large-6 columns colophon">
+                    <h6>Colophon</h6>
+                    <p>This site was hand-built with love and fully <a href="https://github.com/jonokane/jk-folio-15">open-source</a>, in case you're interested in seeing how I built it. I used the following fantastic tools and frameworks:</p>
+                    <ul>
+                        <li>Foundation</li>
+                        <li>jQuery</li>
+                        <li>Masonry</li>
+                        <li>Typekit</li>
+                        <li>Sketch</li>
+                        <li>Adobe CC</li>
+                        <li>Github</li>
+                    </ul>
+                </div>
+                <div class="large-4 columns">
+                    <h6>Social</h6>
+                    <ul>
+                        <li><a href="https://www.linkedin.com/in/jonokane" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">linkedin</span> /jonokane</a></li>
+                        <li><a href="http://www.twitter.com/nokapixel" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">twitter</span> /nokapixel</a></li>
+                        <li><a href="http://facebook.com/jonokane" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">facebook</span> /jonokane</a></li>
+                        <li><a href="http://www.flickr.com/photos/nokapixel" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">flickr</span> /nokapixel</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="medium-12 columns text-center">
+                    <p class="small caps">All work &copy;2015 unless otherwise noted</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- end of .main-bg -->
+    <div id="modal-box" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog"></div>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="bower_components/foundation/js/foundation/foundation.reveal.js"></script>
+    <script src="bower_components/masonry/dist/masonry.pkgd.min.js"></script>
+    <script src="bower_components/slick.js/slick/slick.min.js"></script>
+    <script src="js/ss-social.js"></script>
+    <script src="js/ss-glyphish-outlined.js"></script>
+    <script src="js/app.js"></script>
+    <script>
+// jkcontact
+$(function(){
+    $("#JqAjaxForm").submit(function(e){
+       e.preventDefault();
+
+        dataString = $("#JqAjaxForm").serialize();
+
+        $.ajax({
+        type: "POST",
+        url: "emailer.php",
+        data: dataString,
+        dataType: "json",
+        success: function(data) {
+
+            if(data.email_check == "invalid"){
+                $("#contactMessage").html("<div class='errorMessage'>Sorry " + data.name + ", " + data.email + " isn't a valid email address. Give it another shot. Thanks</div>");
+            } else {
+                $("#contactMessage").html("<div class='successMessage'>Hi "+ data.name + ". Thanks for contacting me. I'll respond to you via email at " + data.email + ". Thank you.</div>");
+                $("#contactForm").hide();
+            }
+
+        }
+
+        });          
+
+    });
+});
+
+
+
+    </script>
+</body>
+
+</html>
