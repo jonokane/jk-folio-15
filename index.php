31688d5c451b4de753dceccb9ff535e4f4d45341
jonokane <jono@jonokane.com>
2015-06-02 14:14:16 -0700

    updating footer with links

--- index.php
+++ index.php
@@ -37,13 +37,6 @@
                     <li><a href="http://www.flickr.com/photos/nokapixel" target="_blank" class="soc-link"><span class="ss-icon ss-social-circle">flickr</span></a></li>
                     <!-- <li><a href="#"><span class="ss-icon ss-social-circle">aboutme</span></a></li> -->
                     <!-- <li><a href="#"><span class="ss-icon ss-social-circle">soundcloud</span></a></li> -->
-                    <!-- <li class="has-dropdown">
-                    <a href="#">Right Button Dropdown</a>
-                    <ul class="dropdown">
-                        <li><a href="#">First link in dropdown</a></li>
-                        <li class="active"><a href="#">Active link in dropdown</a></li>
-                    </ul>
-                </li> -->
                 </ul>
                 <!-- Left Nav Section -->
                 <ul class="left">
@@ -225,13 +218,13 @@
                     <h6>Colophon</h6>
                     <p>This site was hand-built with love and fully <a href="https://github.com/jonokane/jk-folio-15">open-source</a>, in case you're interested in seeing how I built it. I used the following fantastic tools and frameworks:</p>
                     <ul>
-                        <li>Foundation</li>
-                        <li>jQuery</li>
-                        <li>Masonry</li>
-                        <li>Typekit</li>
-                        <li>Sketch</li>
-                        <li>Adobe CC</li>
-                        <li>Github</li>
+                        <li><a href="http://foundation.zurb.com/" target="_blank">Foundation</a></li>
+                        <li><a href="http://jquery.com/" target="_blank">jQuery</a></li>
+                        <li><a href="http://masonry.desandro.com/" target="_blank">Masonry</a></li>
+                        <li><a href="https://typekit.com/" target="_blank">Typekit</a></li>
+                        <li><a href="http://bohemiancoding.com/sketch/" target="_blank">Sketch</a></li>
+                        <li><a href="http://adobe.com" target="_blank">Adobe CC</a></li>
+                        <li><a href="https://github.com/" target="_blank">Github</a></li>
                     </ul>
                 </div>
                 <div class="large-4 columns">
